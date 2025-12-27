9474c95886748daa947edff02227245e617aa3c1
PornoHup <107538335+PornoHup@users.noreply.github.com>
2025-12-27 21:08:47 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-TOKEN = "8361619627:AAHMu7MCqLjQHbEWxb3VUhTRiHGK_C5bxuI"
-OPENAI_API_KEY = "Sənin OpenAI API Key"
-CHAT_ID = "-1003579299647"  # Botun mesaj göndərəcəyi qrup
+import os
+TOKEN = os.getenv("TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+CHAT_ID = os.getenv("CHAT_ID")
