4d15c83235031bfdc74fbc4656561d5e9ef23ad6
PornoHup <107538335+PornoHup@users.noreply.github.com>
2025-12-27 20:44:39 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = "Sənin Telegram Bot Token"
+TOKEN = "8361619627:AAHMu7MCqLjQHbEWxb3VUhTRiHGK_C5bxuI"
 OPENAI_API_KEY = "Sənin OpenAI API Key"
 CHAT_ID = "@sənin_qrup_username"  # Botun mesaj göndərəcəyi qrup
